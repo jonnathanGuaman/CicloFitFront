31c145b2c36e8e6aec495a552ff01dcf866cd332
Jonnathan Guaman <jonnathan.guaman.est@tecazuay.edu.ec>
2024-09-24 07:28:12 -0500

    listo para despelgar

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    urlHost: 'http://ciclofitcontainer:8080/',
-    urlApi: 'http://ciclofitcontainer:8080/api/v1/',
+    urlHost: 'http://204.48.19.2:8080/',
+    urlApi: 'http://204.48.19.2:8080/api/v1/',
     ocualtarRutina:true,
     ocultarFormRutina:true
   };
